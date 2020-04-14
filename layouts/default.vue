caf745abf7192afef8403ff09b853c9de2aa2af1
Francis Claide Magallen <magallen.fc@gmail.com>
2020-04-14 17:37:09 +0800

    Update web app title

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar wrapper-class="container is-fluid" type="is-dark" :fixed-top="true">
       <template slot="brand">
-        <b-navbar-item tag="router-link" to="/">COVID19PINAS</b-navbar-item>
+        <b-navbar-item tag="router-link" to="/">COVIDPH.LIVE</b-navbar-item>
       </template>
       <template slot="start">
         <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
