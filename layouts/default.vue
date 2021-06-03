84f55e6ba2d01f7dcc3553abf225650b37326807
Francis <magallen.fc@gmail.com>
2021-06-03 16:07:18 +0800

    Fix - temporary hide cases and map features

--- layouts/default.vue
+++ layouts/default.vue
@@ -9,8 +9,8 @@
       </template>
       <template slot="start">
         <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
-        <b-navbar-item tag="router-link" to="cases">Cases</b-navbar-item>
-        <b-navbar-item tag="router-link" to="map">Map</b-navbar-item>
+        <!-- <b-navbar-item tag="router-link" to="cases">Cases</b-navbar-item> -->
+        <!-- <b-navbar-item tag="router-link" to="map">Map</b-navbar-item> -->
         <b-navbar-item tag="router-link" to="useful-resources">Useful Resources</b-navbar-item>
         <b-navbar-item tag="router-link" to="about">About</b-navbar-item>
       </template>
